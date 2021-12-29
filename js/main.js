81fd79bbf97aa8e8eca2bb91018b480a2017b96f
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2021-12-29 02:14:50 -0500

    deleted old files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { fetchJordans } from './fetchJordan'
-
-const jordan = document.getElementById('jordan');
-const nike = document.getElementById('nike');
-const yeezy = document.getElementById('yeezy');
-
-jordan.addEventListener('click', () => {
-  fetchJordans();
-})
