d6301e76cb310bc6ffc1c29e1c5c02b1a10c9b8d
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-01-01 19:02:36 -0500

    add padding to mobile cards, new hover method

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,25 @@ const Card = ({ sneaker }) => {
   const bg = {
     backgroundImage: `url(${sneaker.media.imageUrl})`
   }
+
   if (sneaker.media.imageUrl === null) {
     bg.backgroundImage = `url(https://www.freeiconspng.com/uploads/no-image-icon-4.png)`
   }
 
+  const handleMouseEnter = (e) => {
+    const cardParent = e.target.parentNode;
+    const hover = cardParent.querySelector('.info-container');
+
+    hover.style.display = 'flex';
+
+    hover.addEventListener('mouseleave', () => {
+      hover.style.display = 'none';
+    });
+  }
+
   return (
     <span>
-      <div className="card" style={bg}></div>
+      <div className="card" style={bg} onMouseEnter={(e) => handleMouseEnter(e)}></div>
       <a className="info-container" href="https://google.com">
         {sneaker.name}
       </a>
